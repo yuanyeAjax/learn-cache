b597b4b8ff5b62becfecd9a71e01b8270da2ad0a
wb_yuanye <wb_yuanye@meituan.com>
2023-07-12 14:01:28 +0800

    feat: 更新内容

--- server.js
+++ server.js
@@ -18,23 +18,27 @@ const getIP = () => {
 
 http.createServer((req, res) => {
   const url = req.url
-  console.log('--------', url)
+  // console.log('--------', url)
 
   let content  = ''
 
   if (url === '/') {
     content = fs.readFileSync(`${__dirname}/index/index.html`)
   } else {
+    // 获取head.css文件 强制缓存
     if (url === '/img.png' || url === '/head.css') {
       res.setHeader('Cache-Control', 'max-age=10')
     }
+    // 获取index.css文件 协商缓存
     if (url === '/index.css') {
       const info = fs.statSync(`${__dirname}/index${url}`)
 
+      // 获取文件修改时间
       const changeTime = info.mtime.toGMTString()
 
       const ifModifiedSince = req.headers['if-modified-since']
 
+      // 如果浏览器传来的最后一次修改时间 和 文件实际修改时间一致
       if (ifModifiedSince === changeTime) {
         res.statusCode = 304
         return res.end()
